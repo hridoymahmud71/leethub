930c05916d99ce6a3716b7883d0b99a172e70dea
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-09-11 00:02:52 +0600

    Time: 63 ms (26.43%), Space: 13.8 MB (64.55%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,8 +1,35 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        
-        return nums.sort()
+    
+        self.mergesort(nums)
+
+
+    def mergesort(self, nums: List[int]):
+        if len(nums) > 1:
+            mid = len(nums)//2
+            left, right = nums[:mid], nums[mid:]
+
+            self.mergesort(left)
+            self.mergesort(right)
+
+            i = j = k = 0
+
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    nums[k] = left[i]
+                    i+=1
+                else:
+                    nums[k] = right[j]
+                    j+=1
+                k+=1
+
+            while i < len(left):
+                nums[k] = left[i]
+                i+=1
+                k+=1
+
+            while j < len(right):
+                nums[k] = right[j]
+                j+=1
+                k+=1
         
