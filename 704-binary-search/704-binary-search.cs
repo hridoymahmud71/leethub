581b7f63baa9fc4190b1a5e84e6468d3f6144cd8
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-09-15 01:12:01 +0600

    Time: 233 ms (19.04%), Space: 41.5 MB (69.65%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        
+        int minIndex = 0;
+        int maxIndex = nums.Length - 1;
+        
+        while(minIndex <= maxIndex) {
+            int midIndex = (minIndex + maxIndex) / 2 ;
+            
+            if(target == nums[midIndex]){
+                return midIndex;
+            } else if (target < nums[midIndex]){
+                 maxIndex = midIndex - 1;
+            } else {
+                 minIndex  = midIndex + 1;
+            }
+        }
+        
+        return -1;
+        
+    }
+}
