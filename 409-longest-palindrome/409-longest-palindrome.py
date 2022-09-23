eb9fe495e362dc7409054e81126abbc47589c7e7
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-09-23 23:01:00 +0600

    Time: 78 ms (5.96%), Space: 14 MB (22.54%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        keeper = set()
+        counter = 0
+
+        for i in range(len(s)):
+            if s[i] in keeper:
+                counter += 2 # if it existed that means with current item it creates a pair of the item
+                keeper.remove(s[i]) #  remove a paired item
+
+            else:
+                keeper.add(s[i])
+
+        
+        return counter if len(keeper) == 0 else counter + 1
