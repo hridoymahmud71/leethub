5af33edf83c3731a63bb81d4e000a6dbe3c5f292
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-09-25 00:26:41 +0600

    Time: 333 ms (5.08%), Space: 19.8 MB (8.94%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid) - 1
+        n = len(grid[0]) - 1
+        memo = {}
+
+        # -----start-----
+        # recursive way with memo
+
+        def dfsTraverse(m, n, memo):
+
+            memkey = str(m)+"-"+str(n)
+
+            if memkey in memo.keys():
+                return memo[memkey]
+
+            if m == 0 and n == 0:
+                return grid[m][n]
+
+            if m < 0 or n < 0:
+                return float('inf')
+
+            memo[memkey] = min(dfsTraverse(m-1, n, memo),
+                               dfsTraverse(m, n-1, memo)) + grid[m][n]
+            return memo[memkey]
+
+        return  dfsTraverse(m, n, memo)
