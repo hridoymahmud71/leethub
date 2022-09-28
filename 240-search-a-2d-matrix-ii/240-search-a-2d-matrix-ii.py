81b35c7281fa41c262d5972c3f75393ee09173d9
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-09-28 23:51:46 +0600

    Time: 539 ms (5.10%), Space: 21 MB (5.89%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,7 +1,23 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] == target:
-                    return True                
-        return False
+        
+        rows = len(matrix) - 1
+        cols = len(matrix[0]) - 1
+
+        def divideAndConquer(r, c):
+
+            current = matrix[r][c]
+
+            if current == None:
+                return False
+            if current == target:
+                return True
+
+            matrix[r][c] = None
+
+            if current > target:
+                return (r > 0 and divideAndConquer(r - 1, c)) or (c > 0 and divideAndConquer(r, c - 1))
+            else:
+                return (r < rows and divideAndConquer(r + 1, c)) or (c < cols and divideAndConquer(r, c + 1))
+
+        return divideAndConquer(0, 0)
