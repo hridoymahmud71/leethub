e2589dc37dea1985a0ea6384caba84575e4661a9
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-09-28 21:40:19 +0600

    Time: 101 ms (10.34%), Space: 14.3 MB (88.95%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -4,6 +4,5 @@ class Solution:
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == target:
-                    return True
-                
+                    return True                
         return False
