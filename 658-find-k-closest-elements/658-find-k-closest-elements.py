9a9ea9efc01424997a523db032330cf4fd2e64a1
Mahmudur Rahman <hridoymahmud71@gmail.com>
2022-09-29 23:19:04 +0600

    Time: 635 ms (35.43%), Space: 15.6 MB (45.71%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        low = 0
+        high = len(arr) - 1
+
+        while (high - low >= k):
+
+            lowDiff = abs(arr[low] - x)
+            highDiff = abs(arr[high] - x)
+
+            if lowDiff <= highDiff:
+                high -= 1
+            else:
+                low += 1
+
+        print(low,high)
+        output = arr[low:high + 1]
+
+        return output
